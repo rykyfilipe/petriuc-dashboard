47fb585fe64101b31a0af184afe0e633ec6c36d4
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-05-30 13:22:01 +0300

    insatl as app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+/** @format */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
